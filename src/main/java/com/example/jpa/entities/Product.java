1290608b13790c99f76deafd064bc39c7ee7c78e
Sweta Shah <sweta2016shah@gmail.com>
2026-04-06 15:44:41 +0530

    Hibernate ORM,JPA,Entities and Table in Spring Data JPA

---
+++ src/main/java/com/example/jpa/entities/Product.java
@@ -0,0 +1,46 @@
+package com.example.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Product_Table", uniqueConstraints = {
+        @UniqueConstraint(name ="sku_unique",columnNames = {"sku"}),
+        },
+        indexes = {
+        @Index(name = "sku", columnList = "sku")
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private  String sku;
+
+    @Column(name = "title_x")
+    private String title;
+
+    private BigDecimal price;
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO jpa_tutorial.product_table (price,quantity,sku,title_x)
+VALUES
+(12.4,4,'parle7341','parle Biscuit'),
+(19.4,4,'pepsi1231','pepsi'),
+(11.4,4,'pepsi1','maza ');
