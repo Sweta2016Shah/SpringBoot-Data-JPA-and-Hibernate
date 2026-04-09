4e2d41d68745f7cd08dc01ea5732d2dbd4a7a754
Sweta Shah <sweta2016shah@gmail.com>
2026-04-09 15:01:26 +0530

    Persistence Context

---
+++ src/main/java/com/example/jpa/services/PatientService.java
@@ -0,0 +1,26 @@
+package com.example.jpa.services;
+
+import com.example.jpa.entities.Patient;
+import com.example.jpa.repositories.PatientRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    @Transactional
+    public void testPatientTransaction(){
+        Patient p1 = patientRepository.findById(1L).orElseThrow();
+        Patient p2 = patientRepository.findById(1L).orElseThrow();
+
+        System.out.println(p1 + " " + p2);
+        System.out.println(p1==p2);
+
+        p1.setName("Random name");
+    }
+
+}
--- src/test/java/com/example/jpa/PatientServiceTest.java
+++ src/test/java/com/example/jpa/PatientServiceTest.java
@@ -5,6 +5,7 @@ import com.example.jpa.dto.CPatientInfo;
 import com.example.jpa.dto.IPatientInfo;
 import com.example.jpa.entities.Patient;
 import com.example.jpa.repositories.PatientRepository;
+import com.example.jpa.services.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,9 @@ public class PatientServiceTest {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    @Autowired
+    private PatientService patientService;
 
 //    @Test
 //    public void testPatient(){
@@ -39,7 +43,8 @@ public class PatientServiceTest {
 
     @Test
     public void testPatient(){
-        int rowsAffected = patientRepository.updatePatientNameWithId("Anuj Sharma", 1L);
-        System.out.println(rowsAffected);
+//        int rowsAffected = patientRepository.updatePatientNameWithId("Anuj Sharma", 1L);
+//        System.out.println(rowsAffected);
+        patientService.testPatientTransaction();
     }
 }
